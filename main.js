c8bafc2991874f73ac1c23fb39caa5d1fe4ebd0b
Eished <kished@outlook.com>
2021-05-31 00:42:25 +0800

    投票功能异步执行，自动获取投票地址

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jkforum helper
 // @namespace    https://www.jkforum.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  自动签到，自动完成投票任务
 // @author       Eished
 // @license      AGPL-3.0
@@ -42,10 +42,6 @@ function launch() {
   task(urlApply);
   // 签到
   sign();
-  // 投票
-  voted();
-  // 领取投票奖励
-  taskDone(urlDraw);
 }
 
 // 签到 直接post签到数据
@@ -78,15 +74,82 @@ function sign() {
   };
 }
 
+
+// 申请投票任务
+let urlApply = 'https://www.jkforum.net/home.php?mod=task&do=apply&id=59';
+
+function task(urlApply) {
+  var httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
+  httpRequest.open('GET', urlApply, true); //第二步：打开连接  将请求参数写在url中  ps:"./Ptest.php?name=test&nameone=testone"
+  httpRequest.send(); //第三步：发送请求  将请求参数写在URL中
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      console.log("task ok");
+      // 执行获取vid
+      getVid(urlVote);
+    }
+  };
+}
+
+// 自动获取vid和aid
+let urlVote = 'https://www.jkforum.net/plugin.php?id=voted';
+let vid = null;
+let aid = null;
+
+function getVid(urlVote) {
+  var httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
+  httpRequest.open('GET', urlVote, true); //第二步：打开连接
+  httpRequest.send(); //第三步：发送请求  将请求参数写在URL中
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      let data = httpRequest.responseText;
+      // 数据类型转换成 html
+      let htmlData = document.createElement('div');
+      htmlData.innerHTML = data;
+      // 找到链接
+      let href = htmlData.querySelector('.voted a').href;
+      console.log(href);
+      // 分解链接
+      vid = href.split('&')[2];
+      console.log(vid);
+
+      // 获取投票页 aid
+      getAid(href);
+    }
+  };
+}
+
+// 获取aid
+function getAid(vidUrl) {
+  var httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
+  httpRequest.open('GET', vidUrl, true); //第二步：打开连接
+  httpRequest.send(); //第三步：发送请求  将请求参数写在URL中
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      let data = httpRequest.responseText;
+      // 数据类型转换成 html
+      let htmlData = document.createElement('div');
+      htmlData.innerHTML = data;
+      // 找到链接
+      let href = htmlData.querySelector('.hp_s_c a').href;
+      console.log(href);
+      // 分解链接
+      aid = href.split('&')[2];
+      console.log(aid);
+      // 调用投票
+      voted(aid, vid);
+    }
+  };
+}
 // 投票
-function voted() {
+function voted(aid, vid) {
   let formhash = document.querySelectorAll('#scbar_form input')[1].value; //hash 值
   let pMessage = 'formhash=' + formhash + '&inajax=1&handlekey=dian&sid=0&message=1'; //post 报文
-  let aid = 13798; //投票目标
-  let url = 'https://www.jkforum.net/plugin/?id=voted&ac=dian&aid=' + aid + '&vid=1114&qr=&inajax=1'; //请求链接
+  // let aid = 13798; //投票目标
+  let url = 'https://www.jkforum.net/plugin/?id=voted&ac=dian&aid=' + aid + '&' + vid + ' & qr = & inajax = 1 '; //拼接投票链接
 
   var httpRequest = new XMLHttpRequest(); //第一步：创建需要的对象
-  httpRequest.open('POST', url, false); //第二步：打开连接
+  httpRequest.open('POST', url, true); //第二步：打开连接
   httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
   httpRequest.send(pMessage); //发送请求 将情头体写在send中
   httpRequest.onreadystatechange = function () { //请求后的回调接口，可将请求成功后要执行的程序写在其中
@@ -95,26 +158,13 @@ function voted() {
 
       let data = xmlRepo.getElementsByTagName("root")[0].childNodes[0].nodeValue;
       console.log(data);
+      // 执行领奖励
+      taskDone(urlDraw);
       // 提示信息
       alert(data);
     }
   };
 }
-
-// 申请投票任务
-let urlApply = 'https://www.jkforum.net/home.php?mod=task&do=apply&id=59';
-
-function task(urlApply) {
-  var httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
-  httpRequest.open('GET', urlApply, false); //第二步：打开连接  将请求参数写在url中  ps:"./Ptest.php?name=test&nameone=testone"
-  httpRequest.send(); //第三步：发送请求  将请求参数写在URL中
-  httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      console.log("task ok");
-    }
-  };
-}
-
 // 领取投票任务奖励
 let urlDraw = 'https://www.jkforum.net/home.php?mod=task&do=draw&id=59';
 
