f75dcb45dcbd31e99cf5b2c8690b49a9e34493c2
Eished <kished@outlook.com>
2021-06-02 01:39:30 +0800

    合并POST模块函数，优化变量，精简大量代码

--- main.js
+++ main.js
@@ -28,8 +28,7 @@ function addBtns() {
     b.id = 'messageBox';
     return b; //返回修改好的DOM对象
   };
-  // 消息盒子添加到body
-  document.querySelector('body').appendChild(genDiv());
+  document.querySelector('body').appendChild(genDiv()); // 消息盒子添加到body
 
   function genButton(text, foo, id) {
     let b = document.createElement('button'); //创建类型为button的DOM对象
@@ -62,7 +61,6 @@ function addBtns() {
   const input = genElement('textarea', 'inp1', 1, 20);
   status_loginned.insertBefore(input, mnoutbox[1]); //添加按钮到指定位置
 
-
   // 感谢 按钮
   const thkBtn = genButton('感谢/回帖', thankauthor); //设置名称和绑定函数
   status_loginned.insertBefore(thkBtn, mnoutbox[1]); //添加按钮到指定位置
@@ -70,11 +68,10 @@ function addBtns() {
   // 签到按钮
   const btn = genButton('签到/投票', launch); //设置名称和绑定函数
   status_loginned.insertBefore(btn, mnoutbox[1]); //添加按钮到指定位置
-
-
 };
 
 function launch() {
+  let urlApply = 'https://www.jkforum.net/home.php?mod=task&do=apply&id=59';
   // 申请任务
   task(urlApply);
   // 签到
@@ -82,35 +79,16 @@ function launch() {
 }
 
 // 签到 直接post签到数据
-const formhash = document.querySelectorAll('#scbar_form input')[1].value; //hash 值
-const todaysay = '好想睡覺~'; //签到输入内容
-
+const formhash = document.querySelectorAll('#scbar_form input')[1].value; // 全局用户hash值
 function sign() {
+  const todaysay = '好想睡覺~'; //签到输入内容
   let pMessage = 'formhash=' + formhash + '&qdxq=ym&qdmode=1&todaysay=' + turnUrl(todaysay) + '&fastreply=1'; //post 报文
   let url = 'https://www.jkforum.net/plugin/?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1'; //请求链接
-
-  const httpRequest = new XMLHttpRequest(); //第一步：创建需要的对象
-  httpRequest.open('POST', url, true); //第二步：打开连接 false同步 true异步
-  httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
-  httpRequest.send(pMessage); //发送请求 将情头体写在send中
-  httpRequest.onreadystatechange = function () { //请求后的回调接口，可将请求成功后要执行的程序写在其中
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) { //验证请求是否发送成功
-      const data = turnCdata(httpRequest.responseXML); //返回HTML数据或字符串
-      // 如果是html则输出html内容
-      if (checkHtml(data)) {
-        const info = data.querySelector('.c').innerHTML.split('<')[0]; // 解析html，返回字符串
-        messageBox(info, 'none');
-      } else {
-        messageBox(data);
-      }
-    }
-  };
+  // 直接post签到数据
+  postData(url, pMessage, 'sign');
 }
 
-
 // 申请投票任务
-let urlApply = 'https://www.jkforum.net/home.php?mod=task&do=apply&id=59';
-
 function task(urlApply) {
   const httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
   httpRequest.open('GET', urlApply, true); //第二步：打开连接
@@ -118,6 +96,7 @@ function task(urlApply) {
   httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
       messageBox("申请投票任务执行成功！");
+      let urlVote = 'https://www.jkforum.net/plugin.php?id=voted';
       // 执行获取vid
       getVid(urlVote);
     }
@@ -125,10 +104,6 @@ function task(urlApply) {
 }
 
 // 自动获取vid和aid
-let urlVote = 'https://www.jkforum.net/plugin.php?id=voted';
-let vid = null; // 纯数字
-let aid = null; // 纯数字
-
 function getVid(urlVote) {
   const httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
   httpRequest.open('GET', urlVote, true); //第二步：打开连接
@@ -142,71 +117,37 @@ function getVid(urlVote) {
       // 找到链接
       const href = htmlData.querySelector('.voted a').href;
       // 分解链接
-      vid = href.split('&')[2].split('=')[1]; // 纯数字
+      const vid = href.split('&')[2].split('=')[1]; // 纯数字
 
       // 获取投票页 aid
-      getAid(href);
+      getAid(href, vid);
     }
   };
 }
 
 // 获取aid
-function getAid(vidUrl) {
+function getAid(vidUrl, vid) {
   const httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
   httpRequest.open('GET', vidUrl, true); //第二步：打开连接
   httpRequest.send(); //第三步：发送请求  将请求参数写在URL中
   httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      let data = httpRequest.responseText;
+      const data = httpRequest.responseText;
       // 数据类型转换成 html
       let htmlData = document.createElement('div');
       htmlData.innerHTML = data;
       // 找到链接
       const href = htmlData.querySelector('.hp_s_c a').href;
       // 分解链接
-      aid = href.split('&')[2].split('=')[1]; // 纯数字
-      // 调用投票
-      voted(aid, vid);
+      const aid = href.split('&')[2].split('=')[1]; // 纯数字
+      const pMessage = 'formhash=' + formhash + '&inajax=1&handlekey=dian&sid=0&message=1'; //post 投票报文
+      const url = 'https://www.jkforum.net/plugin/?id=voted&ac=dian&aid=' + aid + '&vid=' + vid + ' & qr = & inajax = 1 '; //拼接投票链接
+      postData(url, pMessage, 'voted');
     }
   };
 };
-// <script type="text/javascript" reload="1">if(typeof succeedhandle_dian=='function') {succeedhandle_dian('https://www.jkforum.net/home.php?mod=space&do=notice', '投票成功，感謝您的參與！', {});}hideWindow('dian');showDialog('投票成功，感謝您的參與！', 'notice', null, function () { window.location.href ='https://www.jkforum.net/home.php?mod=space&do=notice'; }, 0, null, null, '', '', null, 6);</script>
 
-// 投票
-function voted(aid, vid) {
-  let pMessage = 'formhash=' + formhash + '&inajax=1&handlekey=dian&sid=0&message=1'; //post 报文
-  // let aid = 13798; //投票目标
-  let url = 'https://www.jkforum.net/plugin/?id=voted&ac=dian&aid=' + aid + '&vid=' + vid + ' & qr = & inajax = 1 '; //拼接投票链接
-
-  const httpRequest = new XMLHttpRequest(); //第一步：创建需要的对象
-  httpRequest.open('POST', url, true); //第二步：打开连接
-  httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
-  httpRequest.send(pMessage); //发送请求 将情头体写在send中
-  httpRequest.onreadystatechange = function () { //请求后的回调接口，可将请求成功后要执行的程序写在其中
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) { //验证请求是否发送成功
-      const data = turnCdata(httpRequest.responseXML); //返回HTML数据或字符串
-      // 如果是html则输出html内容
-      if (checkHtml(data)) {
-        let info = '';
-        if (data.querySelector('.alert_info')) {
-          info = turnCdata(data.querySelector('.alert_info').innerHTML); // 解析html，返回字符串，失败警告
-        } else if (data.querySelector('script')) {
-          info = data.querySelector('script').innerHTML.split(`', `)[1].slice(1); // 解析html，获取字符串，成功消息
-        } else {
-          info = "投票返回HTML数据识别失败: " + data;
-        }
-        messageBox(info, 'none');
-      } else {
-        messageBox(data);
-      }
-      // 执行领奖励
-      taskDone(urlDraw);
-    }
-  };
-}
 // 领取投票任务奖励
-let urlDraw = 'https://www.jkforum.net/home.php?mod=task&do=draw&id=59';
-
 function taskDone(urlDraw) {
   const httpRequest = new XMLHttpRequest(); //第一步：建立所需的对象
   httpRequest.open('GET', urlDraw, true); //第二步：打开连接
@@ -217,8 +158,6 @@ function taskDone(urlDraw) {
     }
   };
 }
-
-
 
 // 消息通知弹窗
 function messageBox(text, setTime) {
@@ -252,21 +191,16 @@ function messageBox(text, setTime) {
   }
 }
 
-
 // 自动感谢帖子
 let fid = null; //回帖帖子用
 let replyMessage = ''; //回复内容
 
 function thankauthor() {
-  // 获取回复内容
-  replyMessage = document.querySelector('#inp1').value;
-  // 油猴脚本存储回帖内容
-  GM_setValue('reply', replyMessage);
-  // 获取当前页地址
-  const currentHref = window.location.href;
+  replyMessage = document.querySelector('#inp1').value; // 获取回复内容
+  GM_setValue('reply', replyMessage); // 油猴脚本存储回帖内容
+  const currentHref = window.location.href; // 获取当前页地址
   if (currentHref.split('-')[0] == 'https://www.jkforum.net/forum') {
-    // 获取板块fid
-    fid = currentHref.split('-')[1];
+    fid = currentHref.split('-')[1]; // 获取板块fid
 
     // 判断当前页是否处于图片模式
     if (document.querySelector('.showmenubox').querySelector('[class="chked"]')) {
@@ -328,7 +262,8 @@ function getThreads(currentHref) {
         // 拼接感谢报文
         const thkData = 'formhash=' + formhash + '&tid=' + tid + '&touser=' + touser + '&touser' + touserUid + '&handlekey=k_thankauthor&addsubmit=true';
         // 执行感谢函数
-        thkThread(thkData);
+        const thkReqUrl = 'https://www.jkforum.net/plugin/?id=thankauthor:thank&inajax=1'; //请求地址
+        postData(thkReqUrl, thkData, 'thk'); //post感谢数据
 
         // 参数
         // 拼接回帖url
@@ -343,34 +278,13 @@ function getThreads(currentHref) {
         randomTime += Math.ceil(Math.random() * 5000) + 11000;
         setTimeout(() => {
           // POST回帖数据 必须间隔10秒以上+随机数1-10
-          postData(replyUrl, replyData);
+          postData(replyUrl, replyData, 'reply');
         }, randomTime);
       };
       messageBox('正在回帖中... 总共需要' + (randomTime / 1000 / 60).toFixed(1) + '分钟！如无需回帖，请关闭/刷新页面。', randomTime);
       setTimeout(() => {
         messageBox("全部回帖完成！", 'none');
       }, randomTime);
-    };
-  };
-};
-
-//post感谢数据
-function thkThread(thkData) {
-  const thkReqUrl = 'https://www.jkforum.net/plugin/?id=thankauthor:thank&inajax=1'; //请求地址
-  const httpRequest = new XMLHttpRequest();
-  httpRequest.open('POST', thkReqUrl, true);
-  httpRequest.setRequestHeader('content-Type', 'application/x-www-form-urlencoded');
-  httpRequest.send(thkData); // post数据
-  httpRequest.onreadystatechange = () => {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      const data = turnCdata(httpRequest.responseXML); //返回HTML数据
-      // 如果是html则输出html内容
-      if (checkHtml(data)) {
-        const info = replaceHtml(data.querySelector('.alert_info').innerHTML); //去除html，返回字符串
-        messageBox(info);
-      } else {
-        messageBox(data);
-      }
     };
   };
 };
@@ -386,32 +300,59 @@ function getData(url) {
 };
 
 // POST数据通用模块,返回XML
-function postData(replyUrl, replyData, type) {
+function postData(replyUrl, replyData, fromId, contentType) {
   // 传输数据类型判断,默认 'application/x-www-form-urlencoded'
-  if (type) {} else {
-    type = 'application/x-www-form-urlencoded';
+  if (contentType) {} else {
+    contentType = 'application/x-www-form-urlencoded';
   }
   const httpRequest = new XMLHttpRequest();
   httpRequest.open('POST', replyUrl, true); //同步写法会时区响应
-  httpRequest.setRequestHeader('content-Type', type);
+  httpRequest.setRequestHeader('content-Type', contentType);
   httpRequest.send(replyData); // post数据
   httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      // 提取Cdata返回html,错误数据则直接输出
-      const replyRespo = turnCdata(httpRequest.responseXML);
-      if (replyUrl.split('?')[0] == 'https://www.jkforum.net/forum.php') {
-        if (!checkHtml(replyRespo)) {
-          // xml错误消息
-          messageBox(replyRespo);
+      const stringOrHtml = turnCdata(httpRequest.responseXML); // 提取Cdata返回html或字符串
+
+      // 判断调用来源，选择对应处理方式
+      if (fromId == 'reply') { //回帖
+        if (checkHtml(stringOrHtml)) { // 确认html
+          const info = stringOrHtml.querySelector('script').innerHTML.split(`, `)[1];
+          messageBox(info.split('，')[0].slice(1) + '，' + info.split('，')[1] + '！'); // 返回html成功消息
         } else {
-          const info = replyRespo.querySelector('script').innerHTML.split(`, `)[1];
-          // 返回html成功消息
-          messageBox(info.split('，')[0].slice(1) + '，' + info.split('，')[1] + '！');
+          messageBox(stringOrHtml); //其它情况直接输出
         }
-        // 返回xml字符串或者html
-        return replyRespo;
+      } else if (fromId == 'sign') { //签到
+        if (checkHtml(stringOrHtml)) { // 确认html
+          const info = stringOrHtml.querySelector('.c').innerHTML.split('<')[0]; // 解析html，返回字符串
+          messageBox(info, 'none');
+        } else {
+          messageBox(stringOrHtml); //其它情况直接输出
+        }
+      } else if (fromId == 'voted') { //投票
+        if (checkHtml(stringOrHtml)) {
+          let info = '';
+          if (stringOrHtml.querySelector('.alert_info')) {
+            info = stringOrHtml.querySelector('.alert_info').innerHTML; // 解析html，返回字符串，失败警告
+          } else if (stringOrHtml.querySelector('script')) {
+            info = stringOrHtml.querySelector('script').innerHTML.split(`', `)[1].slice(1); // 解析html，获取字符串，成功消息
+          } else {
+            info = "投票返回HTML数据识别失败: " + stringOrHtml;
+          }
+          messageBox(info, 'none');
+        } else {
+          messageBox(stringOrHtml); //其它情况直接输出
+        }
+        const urlDraw = 'https://www.jkforum.net/home.php?mod=task&do=draw&id=59';
+        taskDone(urlDraw); // 执行领奖励
+      } else if (fromId == 'thk') {
+        if (checkHtml(stringOrHtml)) {
+          const info = replaceHtml(stringOrHtml.querySelector('.alert_info').innerHTML); //去除html，返回字符串
+          messageBox(info);
+        } else {
+          messageBox(stringOrHtml); //其它情况直接输出
+        }
       } else {
-        console.log(replyRespo);
+        messageBox(stringOrHtml); //其它情况直接输出
       }
     };
   };
@@ -457,16 +398,3 @@ function replaceHtml(txt) {
   // 先reg3,\n特殊符号会影响reg的匹配
   return txt.replace(reg3, '').replace(reg, '').replace(reg2, '');
 }
-// 自动评分
-// Request URL: https://www.jkforum.net/forum.php?mod=misc&action=rate&ratesubmit=yes&infloat=yes&inajax=1
-// content-type: application/x-www-form-urlencoded
-// formhash: 
-// tid: 13684758
-// pid: 140618316
-// referer: https://www.jkforum.net/forum.php?mod=viewthread&tid=13684758&page=0#pid140618316
-// handlekey: rate
-// score1: +1
-// reason: 感謝大大分享
-
-
-// 自动回帖报道专区 normalthread_13694588 normalthread_13704863
