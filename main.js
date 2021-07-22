31bbe61c94571d71d170cce3a55a0265813f1852
Eished <kished@outlook.com>
2021-07-22 21:03:03 +0800

    增加自动任务错误处理；

--- main.js
+++ main.js
@@ -106,82 +106,97 @@
   }
 
   async function setFastReply(user) { // 设置快速回复
-    const htmlData = await getData(user.fastReplyUrl);
-    const options = htmlData.querySelectorAll('#rqcss select option');
-    let fastReply = []; //返回数组
-    options.forEach(option => {
-      if (option.value) { //去掉空值
-        fastReply.push(replaceHtml(option.value)); //去掉需要转义的内容
+    try {
+      const htmlData = await getData(user.fastReplyUrl);
+      const options = htmlData.querySelectorAll('#rqcss select option');
+      let fastReply = []; //返回数组
+      if (options.length) {
+        options.forEach(option => {
+          if (option.value) { //去掉空值
+            fastReply.push(replaceHtml(option.value)); //去掉需要转义的内容
+          }
+        });
+      } else {
+        new messageBox("获取快速回复失败！");
+        return user;
       }
-    });
-    if (fastReply.length) {
-      user.fastReply = fastReply;
-      new messageBox("获取快速回复成功！");
-    } else {
-      new messageBox("获取快速回复失败！");
+      if (fastReply.length) {
+        user.fastReply = fastReply;
+        new messageBox("获取快速回复成功！");
+      } else {
+        new messageBox("获取快速回复失败！");
+      }
+      return user;
+    } catch (e) {
+      console.error(e);
+      return user;
     }
-    return user;
   }
   // 启动
   async function launch() {
-    rePic(); // 启动自动加载原图，自动感谢等；
-    if (user.username) { //验证是否登录 //天变动则签到
-      const now = new NowTime();
-      if (user.today != now.day) {
-        user.today = now.day;
-        sign(); // 签到
+    try {
+      rePic(); // 启动自动加载原图，自动感谢等；
+      if (user.username) { //验证是否登录 //天变动则签到
+        const now = new NowTime();
+        if (user.today != now.day) {
+          user.today = now.day;
+          sign(); // 签到
 
-        await getData(user.applyVotedUrl); // 申请任务
-        const msId = new messageBox("申请投票任务执行成功！正在投票请勿退出页面...", "none");
-        // 投票请求链接
-        const votedUrlParams = urlSearchParams({
-          "id": "voted"
-        }).toString();
-        const htmlData = await getData(user.votedUrl + votedUrlParams);
-        const vidUrl = htmlData.querySelector('.voted a').href; // 找到链接
-        const vid = vidUrl.split('&')[2].split('=')[1]; // 纯数字// 分解链接
+          await getData(user.applyVotedUrl); // 申请任务
+          const msId = new messageBox("申请投票任务执行成功！正在投票请勿退出页面...", "none");
+          // 投票请求链接
+          const votedUrlParams = urlSearchParams({
+            "id": "voted"
+          }).toString();
+          const htmlData = await getData(user.votedUrl + votedUrlParams);
+          const vidUrl = htmlData.querySelector('.voted a').href; // 找到链接
+          const vid = vidUrl.split('&')[2].split('=')[1]; // 纯数字// 分解链接
 
-        const hrefHtmlData = await getData(vidUrl);
-        const aidUrl = hrefHtmlData.querySelector('.hp_s_c a').href; // 找到链接
-        const aid = aidUrl.split('&')[2].split('=')[1]; // 纯数字// 分解链接
-        // 投票请求链接数据
-        const votedParams = urlSearchParams({
-          "id": "voted",
-          "ac": "dian",
-          "aid": aid,
-          "vid": vid,
-          "qr": "",
-          "inajax": 1,
-        }).toString();
-        // Post 数据
-        const votedParamsData = urlSearchParams({
-          "formhash": user.formhash,
-          "inajax": 1,
-          "handlekey": "dian",
-          "sid": 0,
-          "message": user.votedMessage,
-        }).toString();
-        // 投票
-        const votedMessage = await postDataAs(user.votedUrl + votedParams, votedParamsData);
-        if (checkHtml(votedMessage)) {
-          let info = '';
-          if (votedMessage.querySelector('.alert_info')) {
-            info = votedMessage.querySelector('.alert_info').innerHTML; // 解析html，返回字符串，失败警告
-            new messageBox(info);
-          } else if (votedMessage.querySelector('script')) {
-            info = votedMessage.querySelector('script').innerHTML.split(`', `)[1].slice(1); // 解析html，获取字符串，成功消息
-            new messageBox(info);
-            await getData(user.taskDoneUrl); // 执行领奖励
-            new messageBox('领取投票奖励成功！');
+          const hrefHtmlData = await getData(vidUrl);
+          const aidUrl = hrefHtmlData.querySelector('.hp_s_c a').href; // 找到链接
+          const aid = aidUrl.split('&')[2].split('=')[1]; // 纯数字// 分解链接
+          // 投票请求链接数据
+          const votedParams = urlSearchParams({
+            "id": "voted",
+            "ac": "dian",
+            "aid": aid,
+            "vid": vid,
+            "qr": "",
+            "inajax": 1,
+          }).toString();
+          // Post 数据
+          const votedParamsData = urlSearchParams({
+            "formhash": user.formhash,
+            "inajax": 1,
+            "handlekey": "dian",
+            "sid": 0,
+            "message": user.votedMessage,
+          }).toString();
+          // 投票
+          const votedMessage = await postDataAs(user.votedUrl + votedParams, votedParamsData);
+          if (checkHtml(votedMessage)) {
+            let info = '';
+            if (votedMessage.querySelector('.alert_info')) {
+              info = votedMessage.querySelector('.alert_info').innerHTML; // 解析html，返回字符串，失败警告
+              new messageBox(info);
+            } else if (votedMessage.querySelector('script')) {
+              info = votedMessage.querySelector('script').innerHTML.split(`', `)[1].slice(1); // 解析html，获取字符串，成功消息
+              new messageBox(info);
+              await getData(user.taskDoneUrl); // 执行领奖励
+              new messageBox('领取投票奖励成功！');
+            }
+          } else {
+            new messageBox(votedMessage); //其它情况直接输出
           }
-        } else {
-          new messageBox(votedMessage); //其它情况直接输出
+          msId.removeMessage();
+          GM_setValue(user.username, user); //保存当天日// today 初始化
         }
-        msId.removeMessage();
-        GM_setValue(user.username, user); //保存当天日// today 初始化
+      } else {
+        new messageBox('未登录');
       }
-    } else {
-      new messageBox('未登录');
+    } catch (e) {
+      GM_setValue(user.username, user); //保存当天日// today 初始化
+      console.error(e);
     }
   }
 
